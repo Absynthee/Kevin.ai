cfc886afda8a85583f937c61212be30cd68d1b0e
Austin Spillman <151957257+Absynthee@users.noreply.github.com>
2025-01-25 18:15:14 +0000

    Removing test blog posts and updating dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from '@astrojs/tailwind';
+// import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         assetsInclude: ['**/*.Astro'],
       },	
-    site: 'https://absynthee.github.io',
+    site: 'https://kev1n.netlify.app',
     // base: 'Kevin.ai',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [mdx(), sitemap()],
 });
